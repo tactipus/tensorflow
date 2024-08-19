3828c6ee2b6e0a6a1ade956f34b8618c68b1a1e1
A. Unique TensorFlower <gardener@tensorflow.org>
2024-08-19 10:42:50 -0700

    [XLA:GPU] add a test case containing a forward cycle to gpu_p2p_pipeliner_test
    PiperOrigin-RevId: 664874727

--- third_party/xla/xla/service/gpu/gpu_p2p_pipeliner_test.cc
+++ third_party/xla/xla/service/gpu/gpu_p2p_pipeliner_test.cc
@@ -33,6 +33,7 @@ limitations under the License.
 #include "xla/service/hlo_parser.h"
 #include "xla/service/hlo_pass_pipeline.h"
 #include "xla/service/hlo_verifier.h"
+#include "xla/tests/filecheck.h"
 #include "xla/tests/hlo_test_base.h"
 #include "xla/util.h"
 
@@ -151,6 +152,182 @@ TEST_F(GpuP2PPipelinerTest,
   EXPECT_THAT(recv1->ToString(), ::testing::HasSubstr(kPeeledAttr));
   EXPECT_THAT(send2->ToString(), ::testing::HasSubstr(kRotatedAttr));
   EXPECT_THAT(recv2->ToString(), ::testing::HasSubstr(kRotatedAttr));
+}
+
+TEST_F(GpuP2PPipelinerTest, SendRecvForwardCycle) {
+  const char* kHloStr = R"(
+  HloModule test
+
+  while_body {
+    inputs = (u32[], f32[2,2], f32[2,2]) parameter(0)
+    iter = u32[] get-tuple-element(inputs), index=0
+    iter_increment = u32[] constant(1)
+    next_iter = u32[] add(iter, iter_increment)
+    weights = f32[2,2] get-tuple-element(inputs), index=2
+    partition-id = u32[] partition-id()
+    zero = u32[] constant(0)
+    compare = pred[] compare(partition-id, zero), direction=EQ
+    broadcast = pred[2,2] broadcast(compare), dimensions={}
+    data = f32[2,2] get-tuple-element(inputs), index=1
+    after-all = token[] after-all()
+
+    send = (f32[2,2], u32[], token[]) send(data, after-all), channel_id=1,
+      frontend_attributes={
+        _xla_send_recv_pipeline="0",
+        _xla_send_recv_source_target_pairs="{{3,0}}",
+        _xla_send_recv_validation="{{3,10}}"
+      }
+    recv = (f32[2,2], u32[], token[]) recv(after-all), channel_id=1,
+      frontend_attributes={
+        _xla_send_recv_pipeline="0",
+        _xla_send_recv_source_target_pairs="{{3,0}}",
+        _xla_send_recv_validation="{{3,10}}"
+      }
+    recv-done = (f32[2,2], token[]) recv-done(recv), channel_id=1,
+      frontend_attributes={_xla_send_recv_pipeline="0"}, control-predecessors={send}
+    recv-done-data = f32[2,2] get-tuple-element(recv-done), index=0
+    after-all.1 = token[] after-all()
+    send.1 = (f32[2,2], u32[], token[]) send(data, after-all.1), channel_id=2,
+      frontend_attributes={
+        _xla_send_recv_pipeline="1",
+        _xla_send_recv_source_target_pairs="{{0,1},{1,2},{2,3}}",
+        _xla_send_recv_validation="{{0,7},{1,8},{2,9}}"
+      }
+    recv.1 = (f32[2,2], u32[], token[]) recv(after-all.1), channel_id=2,
+      frontend_attributes={
+        _xla_send_recv_pipeline="1",
+        _xla_send_recv_source_target_pairs="{{0,1},{1,2},{2,3}}",
+        _xla_send_recv_validation="{{0,7},{1,8},{2,9}}"
+      }
+    recv-done.1 = (f32[2,2], token[]) recv-done(recv.1), channel_id=2,
+      frontend_attributes={_xla_send_recv_pipeline="1"}, control-predecessors={send.1}
+    recv-done-1-data = f32[2,2] get-tuple-element(recv-done.1), index=0
+    select = f32[2,2] select(broadcast, recv-done-data, recv-done-1-data)
+    matmul = f32[2,2] dot(weights, select),
+      lhs_contracting_dims={1}, rhs_contracting_dims={0}
+
+    ROOT result = (u32[], f32[2,2], f32[2,2]) tuple(next_iter, matmul, weights)
+
+    send-done = token[] send-done(send), channel_id=1,
+      frontend_attributes={_xla_send_recv_pipeline="0"}
+    send-done.1 = token[] send-done(send.1), channel_id=2,
+      frontend_attributes={_xla_send_recv_pipeline="1"}
+  }
+
+  while_cond {
+    inputs = (u32[], f32[2,2], f32[2,2]) parameter(0)
+    iter = u32[] get-tuple-element(inputs), index=0
+    max_iter = u32[] constant(3)
+    ROOT compare = pred[] compare(iter, max_iter), direction=LT
+  }
+
+  ENTRY test_computation {
+    start_iter = u32[] constant(0)
+    input_data = f32[2,2] parameter(0)
+    input_weights = f32[2,2] parameter(1)
+    input = (u32[], f32[2,2], f32[2,2]) tuple(start_iter, input_data, input_weights)
+    while_result = (u32[], f32[2,2], f32[2,2]) while(input), condition=while_cond, body=while_body
+    ROOT data_out = f32[2,2] get-tuple-element(while_result), index=1
+  }
+  )";
+  auto module = ParseAndReturnUnverifiedModule(kHloStr, config_).value();
+  EXPECT_TRUE(RunOptimizer(module.get()).value());
+  EXPECT_TRUE(RunFileCheck(module->ToString(), R"(
+  COM: /* file check for while_body */
+    CHECK: %[[WHILE_BODY:.*]] (param: (u32[], f32[2,2], f32[2,2], (f32[2,2], token[]), (f32[2,2], token[]), /*index=5*/token[], token[], u32[])) -> (u32[], f32[2,2], f32[2,2], (f32[2,2], token[]), (f32[2,2], token[]), /*index=5*/token[], token[], u32[])
+    CHECK-NEXT: %[[PARAM:.*]] = (u32[], f32[2,2]{1,0}, f32[2,2]{1,0}, (f32[2,2]{1,0}, token[]), (f32[2,2]{1,0}, token[]), /*index=5*/token[], token[], u32[]) parameter(0)
+    CHECK: %[[ITER:.*]] u32[] get-tuple-element((u32[], f32[2,2]{1,0}, f32[2,2]{1,0}, (f32[2,2]{1,0}, token[]), (f32[2,2]{1,0}, token[]), /*index=5*/token[], token[], u32[]) %param), index=0
+    CHECK: %[[ITER_INCREMENT:.*]] = u32[] constant(1)
+    CHECK: %[[NEXT_ITER:.*]] = u32[] add(u32[] %[[ITER:.*]], u32[] %[[ITER_INCREMENT:.*]])
+    CHECK: %[[WEIGHTS:.*]] = f32[2,2]{1,0} get-tuple-element((u32[], f32[2,2]{1,0}, f32[2,2]{1,0}, (f32[2,2]{1,0}, token[]), (f32[2,2]{1,0}, token[]), /*index=5*/token[], token[], u32[]) %[[PARAM:.*]]), index=2
+    CHECK: %[[PARTITION:.*]] = u32[] partition-id()
+    CHECK: %[[ZERO:.*]] = u32[] constant(0)
+    CHECK: %[[COMPARE:.*]] = pred[] compare(u32[] %[[PARTITION:.*]], u32[] %[[ZERO:.*]]), direction=EQ
+    CHECK: %[[IS_GPU_0:.*]]= pred[2,2]{1,0} broadcast(pred[] %[[COMPARE:.*]]), dimensions={}
+
+    CHECK: %[[RECV_DONE_BWD_DATA_AND_STATUS:.*]] = (f32[2,2]{1,0}, token[]) get-tuple-element((u32[], f32[2,2]{1,0}, f32[2,2]{1,0}, (f32[2,2]{1,0}, token[]), (f32[2,2]{1,0}, token[]), /*index=5*/token[], token[], u32[]) %[[PARAM:.*]]), index=3
+    CHECK: %[[RECV_DONE_BWD_DATA:.*]] = f32[2,2]{1,0} get-tuple-element((f32[2,2]{1,0}, token[]) %[[RECV_DONE_BWD_DATA_AND_STATUS:.*]]), index=0
+    CHECK: %[[RECV_DONE_FWD_DATA_AND_STATUS:.*]] = (f32[2,2]{1,0}, token[]) get-tuple-element((u32[], f32[2,2]{1,0}, f32[2,2]{1,0}, (f32[2,2]{1,0}, token[]), (f32[2,2]{1,0}, token[]), /*index=5*/token[], token[], u32[]) %[[PARAM:.*]]), index=4
+    CHECK: %[[RECV_DONE_FWD_DATA:.*]] = f32[2,2]{1,0} get-tuple-element((f32[2,2]{1,0}, token[]) %[[RECV_DONE_FWD_DATA_AND_STATUS:.*]]), index=0
+
+    CHECK: %[[DATA:.*]] = f32[2,2]{1,0} select(pred[2,2]{1,0} %[[IS_GPU_0:.*]], f32[2,2]{1,0} %[[RECV_DONE_BWD_DATA:.*]], f32[2,2]{1,0} %[[RECV_DONE_FWD_DATA:.*]])
+    CHECK: %[[MATMUL:.*]] = f32[2,2]{1,0} dot(f32[2,2]{1,0} %[[WEIGHTS:.*]], f32[2,2]{1,0} %[[DATA:.*]]), lhs_contracting_dims={1}, rhs_contracting_dims={0}
+
+    CHECK: %[[RECV_BWD_START:.*]] = token[] after-all()
+    CHECK: %[[RECV_BWD:.*]] = (f32[2,2]{1,0}, u32[], token[]) recv(token[] %[[RECV_BWD_START:.*]]), channel_id=1, frontend_attributes={_xla_send_recv_pipeline="0",_xla_send_recv_source_target_pairs={{[{][{]}}3,0{{[}][}]}},_xla_send_recv_validation={{[{][{]}}2,9{{[}][}]}}}
+    CHECK: %[[RECV_DONE_BWD:.*]] = (f32[2,2]{1,0}, token[]) recv-done((f32[2,2]{1,0}, u32[], token[]) %[[RECV_BWD:.*]]), channel_id=1, frontend_attributes={_xla_send_recv_pipeline="0"}
+    CHECK: %[[RECV_FWD_START:.*]] = token[] after-all()
+    CHECK: %[[RECV_FWD:.*]] = (f32[2,2]{1,0}, u32[], token[]) recv(token[] %[[RECV_FWD_START:.*]]), channel_id=2, frontend_attributes={_xla_send_recv_pipeline="1",_xla_send_recv_source_target_pairs={{[{][{]}}0,1},{1,2},{2,3{{[}][}]}},_xla_send_recv_validation={{[{][{]}}0,6},{0,7},{1,8{{[}][}]}}}
+    CHECK: %[[RECV_DONE_FWD:.*]] = (f32[2,2]{1,0}, token[]) recv-done((f32[2,2]{1,0}, u32[], token[]) %[[RECV_FWD:.*]]), channel_id=2, frontend_attributes={_xla_send_recv_pipeline="1"}
+
+    CHECK: %[[SEND_BWD:.*]] = (f32[2,2]{1,0}, u32[], token[]) send(f32[2,2]{1,0} %[[MATMUL:.*]], token[] %[[RECV_BWD_START:.*]]), channel_id=1, frontend_attributes={_xla_send_recv_pipeline="0",_xla_send_recv_source_target_pairs={{[{][{]}}3,0{{[}][}]}},_xla_send_recv_validation={{[{][{]}}2,9{{[}][}]}}} 
+    CHECK: %[[SEND_DONE_BWD:.*]] = token[] send-done((f32[2,2]{1,0}, u32[], token[]) %[[SEND_BWD:.*]]), channel_id=1, frontend_attributes={_xla_send_recv_pipeline="0"}
+    CHECK: %[[SEND_FWD:.*]] = (f32[2,2]{1,0}, u32[], token[]) send(f32[2,2]{1,0} %[[MATMUL:.*]], token[] %[[RECV_FWD_START:.*]]), channel_id=2, frontend_attributes={_xla_send_recv_pipeline="1",_xla_send_recv_source_target_pairs={{[{][{]}}0,1},{1,2},{2,3{{[}][}]}},_xla_send_recv_validation={{[{][{]}}0,6},{0,7},{1,8{{[}][}]}}}
+    CHECK: %[[SEND_DONE_FWD:.*]] = token[] send-done((f32[2,2]{1,0}, u32[], token[]) %[[SEND_FWD:.*]]), channel_id=2, frontend_attributes={_xla_send_recv_pipeline="1"}
+
+    CHECK %[[ONE:.*]] = u32[] constant(1)
+    CHECK %[[NEXT_NEXT_ITER:.*]] = u32[] add(u32[] %[[NEXT_ITER:.*]], u32[] %[[ONE:.*]])
+    CHECK ROOT %[[OUT:.*]] = (u32[], f32[2,2]{1,0}, f32[2,2]{1,0}, (f32[2,2]{1,0}, token[]), (f32[2,2]{1,0}, token[]), /*index=5*/token[], token[], u32[]) tuple(u32[] %[[NEXT_ITER:.*]], f32[2,2]{1,0} %[[MATMUL:.*]], f32[2,2]{1,0} %[[WEIGHTS:.*]], (f32[2,2]{1,0}, token[]) %[[RECV_DONE_BWD:.*]], (f32[2,2]{1,0}, token[]) %[[RECV_DONE_FWD:.*]], /*index=5*/token[] %[[SEND_DONE_BWD:.*]], token[] %[[SEND_DONE_FWD:.*]], u32[] %[[NEXT_NEXT_ITER:.*]])
+
+  COM: /* file check for while condition */
+    CHECK: %[[WHILE_COND:.*]] (cond_param: (u32[], f32[2,2], f32[2,2], (f32[2,2], token[]), (f32[2,2], token[]), /*index=5*/token[], token[], u32[])) -> pred[] {
+    CHECK-NEXT: %[[COND_PARAM:.*]] = (u32[], f32[2,2]{1,0}, f32[2,2]{1,0}, (f32[2,2]{1,0}, token[]), (f32[2,2]{1,0}, token[]), /*index=5*/token[], token[], u32[]) parameter(0)
+    CHECK: %[[CURRENT_ITER:.*]] = u32[] get-tuple-element((u32[], f32[2,2]{1,0}, f32[2,2]{1,0}, (f32[2,2]{1,0}, token[]), (f32[2,2]{1,0}, token[]), /*index=5*/token[], token[], u32[]) %[[COND_PARAM:.*]]), index=0
+    CHECK: %[[TWO:.*]] = u32[] constant(2)
+    CHECK: ROOT %[[COMPARE:.*]] = pred[] compare(u32[] %[[CURRENT_ITER:.*]], u32[] %[[TWO:.*]]), direction=LT
+
+  COM: /* file check for main function */
+    CHECK: ENTRY %[[TEST_COMPUTATION:.*]] (input_data: f32[2,2], input_weights: f32[2,2]) -> f32[2,2] {
+    CHECK-NEXT: %[[START_ITER:.*]] = u32[] constant(0)
+    CHECK: %[[INPUT_DATA:.*]] = f32[2,2]{1,0} parameter(0)
+
+    COM: /* Set up dummy send and recv */
+    CHECK: %[[RECV_BWD_DUMMY_START:.*]] = token[] after-all()
+    CHECK: %[[RECV_BWD_DUMMY:.*]] = (f32[2,2]{1,0}, u32[], token[]) recv(token[] %[[RECV_BWD_DUMMY_START:.*]]), channel_id=1, frontend_attributes={_xla_send_recv_pipeline="0",_xla_send_recv_source_target_pairs={{[{][{]}}3,0{{[}][}]}},_xla_send_recv_validation="invalid"}
+    CHECK: %[[RECV_DONE_BWD_DUMMY:.*]] = (f32[2,2]{1,0}, token[]) recv-done((f32[2,2]{1,0}, u32[], token[]) %[[RECV_BWD_DUMMY:.*]]), channel_id=1, frontend_attributes={_xla_send_recv_pipeline="0"}
+
+    COM: /* Set up iter 0 */
+    CHECK: %[[RECV_FWD_FIRST_ITER_START:.*]] = token[] after-all()
+    CHECK: %[[RECV_FWD_FIRST_ITER:.*]] = (f32[2,2]{1,0}, u32[], token[]) recv(token[] %[[RECV_FWD_FIRST_ITER_START:.*]]), channel_id=2, frontend_attributes={_xla_send_recv_pipeline="1",_xla_send_recv_source_target_pairs={{[{][{]}}0,1},{1,2},{2,3{{[}][}]}},_xla_send_recv_validation={{[{][{]}}0,0},{1,0},{1,0{{[}][}]}}}
+    CHECK: %[[RECV_DONE_FWD_FIRST_ITER:.*]] = (f32[2,2]{1,0}, token[]) recv-done((f32[2,2]{1,0}, u32[], token[]) %[[RECV_FWD_FIRST_ITER:.*]]), channel_id=2, frontend_attributes={_xla_send_recv_pipeline="1"}
+
+    CHECK: %[[FIRST_ITER_INPUT:.*]] = (u32[], f32[2,2]{1,0}, f32[2,2]{1,0}) tuple(u32[] %[[START_ITER:.*]], f32[2,2]{1,0} %[[INPUT_DATA:.*]], f32[2,2]{1,0} %[[INPUT_WEIGHTS:.*]])
+    CHECK: %[[DATA_TO_SEND:.*]] = f32[2,2]{1,0} get-tuple-element((u32[], f32[2,2]{1,0}, f32[2,2]{1,0}) %[[FIRST_ITER_INPUT:.*]]), index=1
+
+    CHECK: %[[SEND_BWD_DUMMY:.*]] = (f32[2,2]{1,0}, u32[], token[]) send(f32[2,2]{1,0} %[[DATA_TO_SEND:.*]], token[] %[[RECV_DUMMY_START:.*]]), channel_id=1, frontend_attributes={_xla_send_recv_pipeline="0",_xla_send_recv_source_target_pairs={{[{][{]}}3,0{{[}][}]}},_xla_send_recv_validation="invalid"}
+    CHECK: %[[SEND_DONE_BWD_DUMMY:.*]] = token[] send-done((f32[2,2]{1,0}, u32[], token[]) %[[SEND_BWD_DUMMY:.*]]), channel_id=1, frontend_attributes={_xla_send_recv_pipeline="0"}
+
+    CHECK: %[[SEND_FWD_FIRST_ITER:.*]] = (f32[2,2]{1,0}, u32[], token[]) send(f32[2,2]{1,0} %[[DATA_TO_SEND:.*]], token[] %[[RECV_FWD_FIRST_ITER_START:.*]]), channel_id=2, frontend_attributes={_xla_send_recv_pipeline="1",_xla_send_recv_source_target_pairs={{[{][{]}}0,1},{1,2},{2,3{{[}][}]}},_xla_send_recv_validation={{[{][{]}}0,0},{1,0},{1,0{{[}][}]}}}
+    CHECK: %[[SEND_DONE_FWD_FIRST_ITER:.*]] = token[] send-done((f32[2,2]{1,0}, u32[], token[]) %[[SEND_FWD_FIRST_ITER:.*]]), channel_id=2, frontend_attributes={_xla_send_recv_pipeline="1"}
+
+    COM: /* set up main loop, starting from iter 1 */
+    CHECK: %[[ONE_MAIN_FUNC:.*]] = u32[] constant(1)
+    CHECK: %[[LOOP_INPUT:.*]] = (u32[], f32[2,2]{1,0}, f32[2,2]{1,0}, (f32[2,2]{1,0}, token[]), (f32[2,2]{1,0}, token[]), /*index=5*/token[], token[], u32[]) tuple(u32[] %[[START_ITER:.*]], f32[2,2]{1,0} %[[INPUT_DATA:.*]], f32[2,2]{1,0} %[[INPUT_WEIGHTS:.*]], (f32[2,2]{1,0}, token[]) %[[RECV_DONE_BWD_DUMMY:.*]], (f32[2,2]{1,0}, token[]) %[[RECV_DONE_FWD_FIRST_ITER:.*]], /*index=5*/token[] %[[SEND_DONE_BWD_DUMMY:.*]], token[] %[[SEND_DONE_FWD_FIRST_ITER:.*]], u32[] %[[ONE_MAIN_FUNC:.*]])
+
+    CHECK: %[[WHILE:.*]] = (u32[], f32[2,2]{1,0}, f32[2,2]{1,0}, (f32[2,2]{1,0}, token[]), (f32[2,2]{1,0}, token[]), /*index=5*/token[], token[], u32[]) while((u32[], f32[2,2]{1,0}, f32[2,2]{1,0}, (f32[2,2]{1,0}, token[]), (f32[2,2]{1,0}, token[]), /*index=5*/token[], token[], u32[]) %[[LOOP_INPUT:.*]]), condition=%[[WHILE_COND:.*]], body=%[[WHILE_BODY:.*]]
+
+    CHECK: %[[ITER_AFTER_WHILE:.*]] = u32[] get-tuple-element((u32[], f32[2,2]{1,0}, f32[2,2]{1,0}, (f32[2,2]{1,0}, token[]), (f32[2,2]{1,0}, token[]), /*index=5*/token[], token[], u32[]) %[[WHILE:.*]]), index=0
+    CHECK: %[[ITER_INCR:.*]] = u32[] constant(1)
+    CEHCK: %[[LAST_ITER:.*]] = u32[] add(u32[] %[[ITER_AFTER_WHILE:.*]], u32[] %[[ITER_INCR:.*]])
+    CHECK: %[[WEIGHTS_AFTER_WHILE:.*]] = f32[2,2]{1,0} get-tuple-element((u32[], f32[2,2]{1,0}, f32[2,2]{1,0}, (f32[2,2]{1,0}, token[]), (f32[2,2]{1,0}, token[]), /*index=5*/token[], token[], u32[]) %[[WHILE:.*]]), index=2
+
+    CHECK: %[[PARTITION_MAIN_FUNC:.*]] = u32[] partition-id()
+    CHECK: %[[ZERO_MAIN_FUNC:.*]] = u32[] constant(0)
+    CHECK: %[[COMPARE_MAIN_FUNC:.*]] = pred[] compare(u32[] %[[PARTITION_MAIN_FUNC:.*]], u32[] %[[ZERO_MAIN_FUNC:.*]]), direction=EQ
+    CHECK: %[[IS_GPU_0_MAIN_FUNC:.*]] = pred[2,2]{1,0} broadcast(pred[] %[[COMPARE_MAIN_FUNC:.*]]), dimensions={}
+
+    CHECK: %[[RECV_DONE_BWD_STATUS_AFTER_WHILE:.*]] = (f32[2,2]{1,0}, token[]) get-tuple-element((u32[], f32[2,2]{1,0}, f32[2,2]{1,0}, (f32[2,2]{1,0}, token[]), (f32[2,2]{1,0}, token[]), /*index=5*/token[], token[], u32[]) %[[WHILE:.*]]), index=3
+    CHECK: %[[RECV_DONE_BWD_DATA_AFTER_WHILE:.*]] = f32[2,2]{1,0} get-tuple-element((f32[2,2]{1,0}, token[]) %[[RECV_DONE_BWD_STATUS_AFTER_WHILE:.*]]), index=0
+    CHECK: %[[RECV_DONE_FWD_STATUS_AFTER_WHILE:.*]] = (f32[2,2]{1,0}, token[]) get-tuple-element((u32[], f32[2,2]{1,0}, f32[2,2]{1,0}, (f32[2,2]{1,0}, token[]), (f32[2,2]{1,0}, token[]), /*index=5*/token[], token[], u32[]) %[[WHILE:.*]]), index=4
+    CHECK: %[[RECV_DONE_FWD_DATA_AFTER_WHILE:.*]] = f32[2,2]{1,0} get-tuple-element((f32[2,2]{1,0}, token[]) %[[RECV_DONE_FWD_STATUS_AFTER_WHILE:.*]]), index=0
+
+    CHECK: %[[LAST_ITER_COMPUTE_DATA:.*]] = f32[2,2]{1,0} select(pred[2,2]{1,0} %broadcast.2, f32[2,2]{1,0} %[[RECV_DONE_BWD_DATA_AFTER_WHILE:.*]], f32[2,2]{1,0} %[[RECV_DONE_FWD_DATA_AFTER_WHILE:.*]])
+    CHECK: %[[LAST_MATMUL:.*]] = f32[2,2]{1,0} dot(f32[2,2]{1,0} %[[WEIGHTS_AFTER_WHILE:.*]], f32[2,2]{1,0} %[[LAST_ITER_COMPUTE_DATA:.*]]), lhs_contracting_dims={1}, rhs_contracting_dims={0}
+
+    CHECK: %[[ALL_RESULTS:.*]] = (u32[], f32[2,2]{1,0}, f32[2,2]{1,0}) tuple(u32[] %[[ITER_AFTER_WHILE:.*]], f32[2,2]{1,0} %[[LAST_MATMUL:.*]], f32[2,2]{1,0} %[[WEIGHTS_AFTER_WHILE:.*]])
+    CHECK: ROOT %[[DATA_OUT:.*]] = f32[2,2]{1,0} get-tuple-element((u32[], f32[2,2]{1,0}, f32[2,2]{1,0}) %[[ALL_RESULTS:.*]]), index=1
+  )")
+                  .value());
 }
 
 }  // namespace
